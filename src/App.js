b4a6a575dd905b440064c71c71f7d8adf73a9c52
Eddie Jaoude <eddie@jaoudestudios.com>
2021-08-28 22:42:35 +0100

    feat: ui chat

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+
+import React, { useState } from "react";
+
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import { Timeline } from "primereact/timeline";
 
 function App() {
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([
+    { author: "Bot", message: "Chat with your friends" },
+  ]);
+
+  const addMessage = () => {
+    setMessages([...messages, { author: name, message }]);
+    setName("");
+    setMessage("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="p-col-12 p-md-4">
+        <div className="p-inputgroup">
+          <span className="p-inputgroup-addon">
+            <i className="pi pi-user"></i>
+          </span>
+          <InputText
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <InputText
+            placeholder="Chat"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <Button label="Send" onClick={(e) => addMessage()} />
+        </div>
+      </div>
+
+      <div className="card">
+        <h5>Conversation</h5>
+        <Timeline
+          value={messages}
+          opposite={(item) => item.author}
+          content={(item) => (
+            <small className="p-text-secondary">{item.message}</small>
+          )}
+        />
+      </div>
     </div>
   );
 }
